53ce505bf1f311636af5791697396774fa6c7993
Keith Yager <kyager@gmail.com>
2024-12-27 13:11:24 -0500

    Init

---
+++ Testing/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static List<int> GetGrades()
+    {
+        return new List<int> { 85, 90, 78, 92, 88, 43 };
+    }
+
+    public static int GetAverage(List<int> grades)
+    {
+        var result = 0;
+        foreach (var grade in grades)
+        {
+            result += grade;
+        }
+
+        return result / grades.Count;
+    }
+
+    public static void Main(string[] args)
+    {
+        var grades = GetGrades();
+        var result = GetAverage(grades);
+
+        Console.WriteLine($"Average: {result}");
+    }
+}
---
+++ TestingTests/ProgramTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GradesAreEmpty()
+        {
+            // Arrange
+            List<int> grades = Program.GetGrades();
+
+            // Assert
+            Assert.AreNotEqual(0, grades.Count, "No grades were found.");
+        }
+
+        [TestMethod()]
+        public void GradesAreAllPositive()
+        {
+            // Arrange
+            List<int> grades = Program.GetGrades();
+
+            // Act
+            bool allPositive = grades.All(grade => grade > 0);
+
+            // Assert
+            Assert.IsTrue(allPositive, "Not all grades are positive.");
+        }
+
+        [TestMethod()]
+        public void AverageIsCorrect()
+        {
+            // Arrange
+            List<int> grades = new List<int> { 85, 90, 78, 92, 88, 43 };
+
+            // Act
+            int average = Program.GetAverage(grades);
+
+            // Assert
+            Assert.AreEqual(79, average, "Average is incorrect.");
+        }
+    }
+}
